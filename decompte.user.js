5062544676a42b4e1356c90d021a2913c0534f77
Xiao Han <cregnec@users.noreply.github.com>
2016-05-19 13:34:48 +0200

    new version of decompte.user.js
    fill correctly the half-day holiday field

--- decompte.user.js
+++ decompte.user.js
@@ -21,21 +21,51 @@ function eventFire(el, etype){
   }
 }
 
-setTimeout(function() {
-  var dropdown = document.getElementsByClassName('alertbox-content')[0];
-  if(dropdown) {
-    eventFire(dropdown, 'click');
-    setTimeout(function() {
-      var week = document.getElementsByClassName('disclosure-panel')[0].getElementsByTagName('a')[0];
-      if(week) {
-        eventFire(week, 'click');
-        setTimeout(function() {
-          eventFire(document.getElementById('gwt-uid-1'), 'click');
-          setTimeout(function() {
-            eventFire(document.getElementsByClassName('button-action-primaire')[0], 'click');
-          }, 1000);
-        }, 1000);
+function validate(week){
+  eventFire(week, 'click');
+  tbody = document.getElementsByTagName('tbody')[0];
+  if (tbody){
+    trs = tbody.children;
+    for (var i=0; i<trs.length; i++){
+      motif = trs[i].children[4].children[0].innerHTML;
+      //alert(motif);
+      if(motif == '½ CA'){
+        //alert('half day');
+        inputs = trs[i].children[2].getElementsByTagName('input');
+        //hour
+        inputs[0].value = "3";
+        eventFire(inputs[0], 'input');
+        //min
+        inputs[1].value = "48";
+        eventFire(inputs[1], 'input');
+        eventFire(inputs[0], 'change');
       }
-    }, 1000);
+    }
   }
-}, 2000);
+  setTimeout(function() {
+    eventFire(document.getElementById('gwt-uid-1'), 'click');
+  }, 2000);
+  button = document.getElementsByClassName('button-action-primaire')[0];
+  if (button){
+    setTimeout(function() {
+      eventFire(button, 'click');
+    }, 2200);
+  }
+}
+
+function decompte() {
+  setTimeout(function(){
+    var dropdown = document.getElementsByClassName('alertbox-content')[0];
+    if (dropdown){
+      eventFire(dropdown, 'click');
+      setTimeout(function() {
+        var week = document.getElementsByClassName('disclosure-panel')[0].getElementsByTagName('a')[0];
+        if (week){
+          validate(week);
+        }
+      }, 1000);
+    }
+  }, 1000);
+}
+
+decompte();
